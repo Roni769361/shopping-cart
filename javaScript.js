8b88e1fb8bc0356fa5cd63b3b64182e1c9b0429d
Roni769361 <60136750+Roni769361@users.noreply.github.com>
2022-07-21 20:17:15 +0600

    InnerTextError

--- javaScript.js
+++ javaScript.js
@@ -1,61 +1,82 @@
-document.getElementById("caseplusBtn").addEventListener("click", function(){
-    const caseInputSet = document.getElementById("CaseInputText");
-    const caseInputSetNum = parseFloat(caseInputSet.value);
-    const caseInto = caseInputSetNum +1;
-    caseInputSet.value = caseInto;
+// document.getElementById("PhonePluseBtn").addEventListener("click", function(){
+//     // const PhoneInput = document.getElementById("PhoneInput");
+//     // const PhoneInputNo = parseFloat(PhoneInput.value);
+//     // const Number = PhoneInputNo +1;
+//     // PhoneInput.value = Number;
 
-    priceTakaInto("Price", caseInto);
+//     // const PhonePrice = document.getElementById("PhonePrice");
+//     // const PriceValue = Number * 1219;
+//     // PhonePrice.innerText = PriceValue;
+//     ItemPriceUpAndDown("PhoneInput", true);
+// })
 
-})
+// document.getElementById("PhoneMinusBtn").addEventListener("click", function(){
+//     // const PhoneInput = document.getElementById("PhoneInput");
+//     // const PhoneInputNo = parseFloat(PhoneInput.value);
+//     // const Number = PhoneInputNo -1;
+//     // PhoneInput.value = Number;
 
+//     // const PhonePrice = document.getElementById("PhonePrice");
+//     // const PriceValue = Number * 1219;
+//     // PhonePrice.innerText = PriceValue;
+//     ItemPriceUpAndDown("PhoneInput", false);
+// })
 
-document.getElementById("caseMinusBtn").addEventListener("click", function(){
-    const caseInputSet = document.getElementById("CaseInputText");
-    const caseInputSetNum = parseFloat(caseInputSet.value);
-    const caseInto = caseInputSetNum -1;
-    caseInputSet.value = caseInto;
+function ItemPriceUpAndDown(id, price, Isincrease){
+    const PhoneInput = document.getElementById(id);
+    const PhoneInputNo = parseFloat(PhoneInput.value);
+    // const Number = PhoneInputNo -1;
+    let Number = PhoneInputNo;
+    if(Isincrease == false && PhoneInputNo >0){
+        Number = PhoneInputNo -1;
+    }
+    else if(Isincrease == true){
+        Number = PhoneInputNo +1;
+    }
+    PhoneInput.value = Number;
 
-    priceTakaInto("Price", caseInto);
-    
+    const PhonePrice = document.getElementById(price);
+    // const PriceValue = Number * 1219;
+    let PriceValue = Number;
+    if(id == "PhoneInput"){
+        PriceValue = Number * 1219;
+    }
+    else if(id == "CaseInput"){
+        PriceValue = Number * 59;
+    }
+    PhonePrice.innerText = PriceValue;
+    CalculateTotal();
+}
 
-})
+function CalculateTotal(){
+    const PhoneCountNo = GetInputValu("PhoneInput");
 
-document.getElementById("plusBtnSecond").addEventListener("click", function(){
-    const caseInputSet = document.getElementById("CaseInputTextSecond");
-    const caseInputSetNum = parseFloat(caseInputSet.value);
-    const caseInto = caseInputSetNum +1;
-    caseInputSet.value = caseInto;
+    // const CaseCount = document.getElementById('CaseInput');
+    // const CaseCountNo = parseFloat(CaseCount.value);
+    const CaseCountNo = GetInputValu('CaseInput');
 
-    
-    priceTakaIntoSecond("PriceSecond", caseInto);
+    const Subtotal = PhoneCountNo * 1219 + CaseCountNo * 59;
+    const SubtotalText = document.getElementById("subtotaltaka");
+    SubtotalText.innerText = Subtotal;
 
+    const vat = Math.round(Subtotal * 0.1)
+    const vatno = parseFloat(vat)
+    document.getElementById("texttaka").innerText = vatno;
 
-})
+    const subHolding = SubtotalText.innerText;
+    const GreandTotal = subHolding + vatno;
+    const GrandTotalNo = parseFloat(GreandTotal);
+    console.log(GrandTotalNo);
+    // document.getElementById("GreandTotal").innerText = GrandTotal;
 
-document.getElementById("minusBtnSecond").addEventListener("click", function(){
-    const caseInputSet = document.getElementById("CaseInputTextSecond");
-    const caseInputSetNum = parseFloat(caseInputSet.value);
-    const caseInto = caseInputSetNum -1;
-    caseInputSet.value = caseInto;
-    
-    priceTakaIntoSecond("PriceSecond", caseInto);
-
-
-})
-
-function priceTakaIntoSecond(id, caseIntosecond){
-    const priceTaka = document.getElementById(id);
-    const priceTakaUp = caseIntosecond * 59;
-    priceTaka.innerText = priceTakaUp;
 
 }
 
+function GetInputValu(id){
 
-function priceTakaInto(id, caseInto){
-    const priceTaka = document.getElementById(id);
-    const priceTakaUp = caseInto * 1219;
-    priceTaka.innerText = priceTakaUp;
+    const ProductInputCount = document.getElementById(id);
+    const ProductInputCountNo = parseFloat(ProductInputCount.value);
+    return ProductInputCountNo;
+
 
 }
-
-
